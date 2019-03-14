844274d1b586d5a7cc7367447e382d17d3ea30fe
John Farrell <johnjfarrell20@gmail.com>
2019-03-14 16:10:06 -0400

    Login/account creation skeleton

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup'),
     path('', views.login, name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from django.urls import reverse_lazy
+
+from .forms import BeeUserCreationForm
 
 # Create your views here.
 def login(request):
     return HttpResponse("Hello, you're at the login page.")
+
+class SignUp(generic.CreateView):
+
+    form_class = BeeUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
