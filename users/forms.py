24236b91107d2a5fdab9b7dc29e8151d8ec75833
John Farrell <johnjfarrell20@gmail.com>
2019-03-19 15:50:06 -0400

    Custom user signup form

--- users/forms.py
+++ users/forms.py
@@ -3,10 +3,16 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import BeeUser
 
 class BeeUserCreationForm(UserCreationForm):
+    """
+    Custom form for user creation
+    """
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    email = forms.EmailField(max_length=254, required=True)
 
     class Meta(UserCreationForm):
         model = BeeUser
-        fields = ('username', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
 class BeeUserChangeForm(UserChangeForm):
 
--- users/views.py
+++ users/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth import login, authenticate
 
 from .forms import BeeUserCreationForm
 
 # Create your views here.
 def login(request):
     return HttpResponse("Hello, you're at the login page.")
+
+def signup(request):
+    if request.method == 'POST':
+        form = BeeUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    
+    else:
+        form = BeeUserCreationForm()
+    
+    return render(request, 'signup.html', {"form": form})
+        
 
 class SignUp(generic.CreateView):
 
