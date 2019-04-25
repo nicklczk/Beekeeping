19eb8809d22cb9d51df2296c91bc26fb77465a9d
Nicholas Luczak <nick@Nicholass-MacBook-Pro.local>
2019-04-25 19:48:22 -0400

    changed postgres settings to not specify user

--- Beekeeping/settings.py
+++ Beekeeping/settings.py
@@ -81,8 +81,6 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "beekeeping",
-        "USER": "beekeepinguser",
-        "PASSWORD": 12345,
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
